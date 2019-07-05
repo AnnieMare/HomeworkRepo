92e413b0bcc11b77f566f1f6e075af7519840cd1
AnnieMare <mareannie@gmail.com>
2019-07-05 11:00:12 +0200

    05/07/2019 11:00
    change in css

--- Homework7/Homework7/Controllers/ShopController.cs
+++ Homework7/Homework7/Controllers/ShopController.cs
@@ -14,13 +14,7 @@ namespace Homework7.Controllers
         // GET: Shop
         public ActionResult Index()
         {
-            ShopItemViewModel Item1 = new ShopItemViewModel("Belt", "Leather WW2 Nazi belt", 500, 5);
-            ShopItemViewModel Item2 = new ShopItemViewModel("Tunic", "Officers Tunic ww2", 800, 3);
-            ShopItemViewModel Item3 = new ShopItemViewModel("K98 Rifle", "Rifle with wooden stock", 1500, 7);
-            ShopItemList.Add(Item1);
-            ShopItemList.Add(Item2);
-            ShopItemList.Add(Item3);
-            return RedirectToAction("ViewShop");
+            return View();
         }
 
         //view to display shop 
@@ -39,12 +33,29 @@ namespace Homework7.Controllers
         //Add item to ShopViewModel list of items
         public ActionResult SaveNewItem(string ItemName, string ItemDescription, double ItemPrice, int QuantityAvailable)
         {
-            ShopItemViewModel NewItem = new ShopItemViewModel(ItemName, ItemDescription, ItemPrice, QuantityAvailable);
+             ShopItemViewModel NewItem = new ShopItemViewModel(ItemName, ItemDescription, ItemPrice, QuantityAvailable);
             ShopItemList.Add(NewItem);
 
 
             return RedirectToAction("ViewShop");
         }
+
+        //public ActionResult SortByName()
+        //{
+
+        //}
+        //public ActionResult SortByPrice()
+        //{
+
+        //}
+        //public ActionResult SortByAvail()
+        //{
+
+        //}
+        //public ActionResult SortByName()
+        //{
+
+        //}
 
     }
    
